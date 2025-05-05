dfe1c10da9498ee2b183ee0f1b667f1b20762d0f
Juliemelich <julie.melicharova@lemonitech.com>
2025-05-05 12:25:54 +0200

    První nahrání projektu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [quote, setQuote] = useState(""); // výchozí quote nechám prázdné
+
+  const url = "https://api.chucknorris.io/jokes/random";
+
+  const getQuote = async () => {
+    const response = await fetch(url); // vytáhnu citát z url jako JSON
+    const data = await response.json(); // do data uložím citát jako objekt
+    const finalQuote = data["value"]; // do finalQuote uložím samotný citát, který je v objektu jako "value"
+    setQuote(finalQuote); // finalQuote nastavím jako setQuote pro useState
+  };
+
+  useEffect(() => {
+    getQuote(); // zavolám funkci - načte citát
+
+    const interval = setInterval(() => {
+      getQuote(); // každých 10 vteřin načti nový citát
+    }, 15000); // 15 000 milisekund = 10 vteřin
+
+    return () => clearInterval(interval); // úklid, když se komponenta zruší
+  }, []); // funkce se spustí pouze jednou po refreshi
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      <h1>Vtipy o Chucku Norrisovi</h1>
+      <h2>{quote}</h2>
+      <button onClick={getQuote}>Další vtip</button>
+    </div> // přidám tlačítko pro další vtip
   );
-}
+};
 
 export default App;
